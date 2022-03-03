cb7ee622674a53cfc4c9a4be6a68849be96bebfa
nbhung58 <84171951+nbhung58@users.noreply.github.com>
2022-03-03 12:02:29 +0700

    day 8

---
+++ day8/day8-start.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print("Hello World")
+  print("This is function!")
+  print("You are hurted")
+
+#greet()
+
+def greet_with_name(name, location):    
+  print(f"Hello {name}")
+  print(f"I live in {location}")
+
+greet_with_name(location = "Danang", name = "Angela")
